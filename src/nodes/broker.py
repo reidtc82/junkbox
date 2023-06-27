0dfceb1a4409ed7b303ac9d3a787b1c5083002e4
Reid <reidtc82@gmail.com>
2023-06-27 17:21:30 +0000

    adding broker tests

--- src/nodes/broker.py
+++ src/nodes/broker.py
@@ -4,6 +4,10 @@ import threading
 
 
 class JunkBoxServer:
+    """A simple TCP server that listens for incoming connections and
+    sends a response back to the client.
+    """
+
     def __init__(self, host, port):
         self.host = host
         self.port = port
@@ -11,63 +15,55 @@ class JunkBoxServer:
         self.is_running = False
 
     def start(self):
-        # Create a socket object
+        """Starts the server"""
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
-            # Bind the socket to the server address and port
             self.server_socket.bind((self.host, self.port))
 
-            # Listen for incoming connections
             self.server_socket.listen(5)
             print("Server is running and listening for connections...")
             self.is_running = True
 
             while self.is_running:
-                # Wait for a client to connect
                 client_socket, client_address = self.server_socket.accept()
 
-                # Start a new thread to handle the client
                 client_thread = threading.Thread(
                     target=self.handle_client, args=(client_socket, client_address)
                 )
                 client_thread.start()
 
         except KeyboardInterrupt:
-            # Handling Keyboard Interrupt (Ctrl+C)
             print("Server shutdown initiated by keyboard interrupt...")
             self.stop()
             sys.exit(1)
-        except Exception as e:
-            # Handling other exceptions
-            print("An error occurred:", str(e))
+        except Exception as exception:
+            print("An error occurred:", str(exception))
             self.stop()
             sys.exit(1)
 
     def handle_client(self, client_socket, client_address):
+        """Handles a client connection"""
         print("Connected to:", client_address)
 
         while True:
-            # Receive data from the client
             data = client_socket.recv(1024).decode("utf-8")
             print("Received data:", data)
 
-            # If the client sends an empty message, disconnect
             if not data:
                 break
 
             # Process the data (you can implement your own logic here)
 
-            # Send a response back to the client
             if client_socket.fileno() != -1:
                 response = "Hello from the server!"
                 client_socket.sendall(response.encode("utf-8"))
 
-        # Close the connection with the client
         client_socket.close()
         print("Disconnected from:", client_address)
 
     def stop(self):
+        """Stops the server"""
         if self.server_socket:
             self.is_running = False
             self.server_socket.close()
@@ -76,7 +72,6 @@ class JunkBoxServer:
 
 
 if __name__ == "__main__":
-    # Usage example
     server = JunkBoxServer("localhost", 8000)
     server.start()
 
--- tests/test_broker.py
+++ tests/test_broker.py
@@ -1,35 +1,87 @@
-# file to test the broker node 
+# file to test the broker node
 # This should use the broker.py file to test the broker node
 # this should use pytest to test the broker node
 
-import pytest
+import socket
 
-from junk_box.src.nodes import Broker
+from junkbox.src.nodes.broker import JunkBoxServer
+
+SERVER_IP = "localhost"
+SERVER_PORT = 8000
 
 
 def test_broker():
-    # test the broker node
-    # create a broker node
-    broker = Broker()
-    # test the broker node
+    """Test the broker node can be created"""
+
+    broker = JunkBoxServer(SERVER_IP, SERVER_PORT)
+
     assert broker is not None
+    assert broker.host == SERVER_IP
+    assert broker.port == SERVER_PORT
 
-# test that the broker node can be started
+
 def test_broker_start():
-    # create a broker node
-    broker = Broker()
-    # start the broker node
-    broker.start()
-    # determine if the broker node is running
-    assert broker.is_running == True
+    """Test that the broker node can be started"""
 
-# test that the broker node can be stopped
-def test_broker_stop():
-    # create a broker node
-    broker = Broker()
-    # start the broker node
+    broker = JunkBoxServer(SERVER_IP, SERVER_PORT)
     broker.start()
-    # stop the broker node
+
+    assert broker.is_running is True
+
+
+def test_broker_stop():
+    """Test that the broker node can be stopped"""
+
+    broker = JunkBoxServer(SERVER_IP, SERVER_PORT)
+    broker.start()
     broker.stop()
-    # determine if the broker node is running
-    assert broker.is_running == False
+
+    assert broker.is_running is False
+
+
+def test_broker_restart():
+    """Test that the broker node can be restarted"""
+    broker = JunkBoxServer(SERVER_IP, SERVER_PORT)
+    broker.start()
+    broker.stop()
+    broker.start()
+
+    assert broker.is_running is True
+
+
+def test_broker_socket():
+    """Test that the broker node has a socket object"""
+    broker = JunkBoxServer(SERVER_IP, SERVER_PORT)
+    broker.start()
+
+    assert broker.server_socket is not None
+
+
+def test_broker_socket_close():
+    """Test that the broker closes the socket when stopped"""
+    broker = JunkBoxServer(SERVER_IP, SERVER_PORT)
+    broker.start()
+    broker.stop()
+
+    assert broker.server_socket is None
+
+
+def test_broker_socket_restart():
+    """Test that the broker restarts the socket when restarted"""
+    broker = JunkBoxServer(SERVER_IP, SERVER_PORT)
+    broker.start()
+    broker.stop()
+    broker.start()
+
+    assert broker.server_socket is not None
+
+
+def test_socket_connect():
+    """Test that the broker can accept a connection"""
+    broker = JunkBoxServer(SERVER_IP, SERVER_PORT)
+    broker.start()
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((SERVER_IP, SERVER_PORT))
+
+    assert client_socket is not None
